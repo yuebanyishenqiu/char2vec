6ceb91b4404ea470ae4ba9bfe620323cf9972828
yuebanyishenqiu <thisispwj@outlook.com>
2019-12-17 19:44:09 +0800

    first push

--- src/data_helper.py
+++ src/data_helper.py
@@ -1,2 +1,175 @@
 # TODO
 # prepare data for model training
+from zhconv import convert
+from PIL import ImageFont
+import numpy as np
+import json
+from sklearn import preprocessing
+import os
+import sys
+import codecs
+import pickle
+import h5py
+
+
+#default_font_size = 12
+#font_list = ["clerical_script/方正古隶繁体.ttf"]
+#src = "/disk2/pwj/workspace/data/chinese_scripts/"
+#font = ImageFont.truetype(os.path.join(src, font_list[0]), default_font_size)
+#
+#
+#print(font)
+
+def load_json(fin):
+    fp =  codecs.open(fin, "r", encoding = "utf8") 
+    dic = json.load(fp)
+    fp.close()
+
+    return dic
+
+def write2json(fout, dic):
+    fp = codecs.open(fout, "w", encoding = "utf8")
+    json.dump(dic, fp, ensure_ascii = False)
+    fp.close()
+
+def add2vocab(data):
+    
+    vocab = set()
+    text = ""
+    for poem in data:
+        paragraphs = poem["paragraphs"]
+        text = "{} <p>".format(text)
+        for line in paragraphs:
+            line = line.replace(" ", "")
+            for ch in line:
+                vocab.add(ch)
+                text = "{} {}".format(text, ch)
+        text = "{} </p>\n".format(text)
+    return vocab, text
+
+def make_dictionary(vocab, fout):
+
+    dic = {"char2idx": {}, "idx2char": {}}
+    vocab = list(vocab)
+    for i in range(len(vocab)):
+        ch = vocab[i]
+        dic["char2idx"][ch] = i
+        dic["idx2char"][i] = ch
+
+    write2json(fout, dic)
+
+def tokenize_poem(src):
+    # vocab includs three parts:
+    # (1) ordinary chinese word
+    # (2) ， 。 ？ markers
+    # (3) paragraphs initial and final: <p> </p>
+    X = ""
+    vocab = set()
+    for f in os.listdir(src):
+        if f.startswith("ci.song"):
+            fin = "{}/{}".format(src, f)
+            data = load_json(fin)
+            v, t = add2vocab(data)
+            X = "{} {}".format(X, t)
+            vocab |= v
+    
+    vocab |= set(["<p>", "</p>"])
+    fout = "../data/dictionary.json"
+    make_dictionary(vocab, fout)
+    fout = "../data/train.txt"
+    fp = codecs.open(fout, "w", encoding = "utf8")
+    fp.write(X)
+    fp.close()
+
+def vocab_embedding(idx2char, font, font_size = 24, use_traditional = False, normalize = False):
+    r = np.array([make_char_embedding(i, font, use_traditional, idx2char) for i in range(len(idx2char))])
+    return (r - np.mean(r))/np.std(r) if use_traditional else r
+
+def pad_mask(mask, font_size):
+
+    padded_mask = []
+    for l in mask:
+        padded_mask.append(l.tolist()+[0]*(font_size+1-len(l)))
+    for i in range(font_size+1-len(padded_mask)):
+        padded_mask.append([0]*(font_size+1))
+    return np.array(padded_mask)[:font_size+1, : font_size+1]
+
+
+def embed_char(char, font):
+    mask = font.getmask(char)
+    size = mask.size[::-1]
+    a = np.asarray(mask).reshape(size)
+    return a
+
+
+def make_char_embedding(idx, font, use_traditional, idx2char):
+    idx = str(idx)
+    char = idx2char[idx]
+    feat = ""
+    if len(char) > 1:
+        feat =  np.zeros((font.size+1, font.size+1))
+    else:
+        feat =  pad_mask(embed_char(char, font), font.size)
+    
+    vec = []
+    #TODO
+    # make the feature distinct using binary numeric system
+    base = np.arange(-6,7,1)
+    base = np.exp2(base)
+    for v in feat:
+        val = 0
+        for i in range(len(v)):
+            val += base[i]*v[i]
+        vec.append(val)
+    return np.array(vec)
+
+def normalize_data(dic, nomalizer):
+    new_dic = {}
+    for k in dic:
+        feat = [dic[k]]
+        new_feat = nomalizer.transform(feat)
+        new_dic[k] = new_feat
+    return new_dic
+
+def init_char_embedding(f):
+    
+    dic = load_json(f)
+    idx2char = dic["idx2char"]
+    char2idx = dic["char2idx"]
+    
+    font_file = "/disk2/pwj/workspace/data/chinese_scripts/regular_script/STXINGKA.TTF"
+    font = ImageFont.truetype(font_file, 12)
+    emb  = vocab_embedding(idx2char, font, 24)
+    print(emb.shape)
+
+    #fout = "../data/init_char_embedding.pickle"
+    fdic = {}
+    X = np.zeros((len(idx2char), 13))
+    for idx in range(len(idx2char)):
+        feat = make_char_embedding(idx, font, False, idx2char)
+        X[idx,:] = feat
+    print(X.shape)
+    X = preprocessing.Normalizer().fit_transform(X)
+    fout = "../data/wvec.hf"
+    hdf = h5py.File(fout, "w")
+    dst = hdf.create_dataset("data", data = X, dtype = "float")
+    hdf.close()
+#    return 
+#    char = idx2char[str(idx)]
+#    char_idx = char2idx[char]
+#    fdic[char_idx] = feat
+#    from sklearn import preprocessing
+#    nomalizer = preprocessing.Normalizer().fit(X)
+#    fdic = normalize_data(fdic, nomalizer)
+#    fp = codecs.open(fout, "wb")
+#    pickle.dump(fdic, fp, protocol = pickle.HIGHEST_PROTOCOL)
+#    fp.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} ci_dir".format(sys.argv[0]))
+        exit(0)
+
+    tokenize_poem(sys.argv[1])
+    init_char_embedding("../data/dictionary.json")
--- src/main.py
+++ src/main.py
@@ -1,2 +1,163 @@
 # TODO
 # word embedding implementation using pytorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import urllib.request
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+import sklearn
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
+import codecs
+from data_helper import load_json
+import h5py
+
+
+torch.manual_seed(42)
+
+CONTEXT_SIZE = 3
+EMBEDDING_DIM = 13
+
+def load_data(fin):
+    fp = codecs.open(fin, "r", encoding = "utf8")
+    data = fp.readlines()
+    fp.close()
+    X = ""
+    for line in data:
+        line = line.strip()
+        X = "{} {}".format(X, line)
+    return X
+
+
+def get_key(word_id):
+    for key, val in char2idx.items():
+        if(val == word_id):
+            print(key)
+
+def cluster_embeddings(fin, nclusters):
+    X = np.load(fin)
+    kmeans = KMeans(n_clusters = nclusters, random_state = 0).fit(X)
+    center = kmeans.cluster_centers_
+    distances = euclidean_distances(X, center)
+
+    for i in np.arange(0, distances.shape[1]):
+        word_id = np.argmin(distances[:,i])
+        print(word_id)
+        get_key(word_id)
+
+def read_data(fpath):
+
+    tokenizer = RegexpTokenizer(r'\w+')
+    data = urllib.request.urlopen(fpath)
+    data = data.read().decode('utf8')
+    tokenize_data = word_tokenize(data)
+    stop_words = set(stopwords.words('english'))
+    cleaned_words = [i for i in tokenize_data if i not in stop_words]
+    return(cleaned_words)
+
+
+class CBOW(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim, context_size):
+        super(CBOW, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.linear1 = nn.Linear(context_size * embedding_dim, 64)
+        self.linear2 = nn.Linear(64, vocab_size)
+
+    def forward(self, inputs):
+
+        embeds = self.embeddings(inputs).view((1,-1))
+        out1 = F.relu(self.linear1(embeds))
+        out2 = self.linear2(out1)
+
+        log_probs = F.log_softmax(out2, dim=1)
+        return log_probs
+
+
+    def predict(self, inputs):
+        res = self.forward(inputs)
+        res_arg = torch.argmax(res)
+        res_val, res_ind = res.sort(descending=True)
+        res_val = res_val[0][:3]
+        res_ind = res_ind[0][:3]
+
+        for arg in zip(res_val, res_ind):
+            print([(key, val, arg[0]) for key, val in char2idx.items() if val == arg[1]])
+
+
+    def freeze_layer(self, layer):
+        for name, child in model.named_children():
+            print(name, child)
+            if(name == layer):
+                for names, params in child.named_parameters():
+                    print(names, params)
+                    print(params.size())
+                    params.requires_grad = False
+
+    def print_layer_parameters(self):
+        for name, child in model.named_children:
+            print(name, child)
+            for name, params in child.named_parameters():
+                print(names, params)
+                print(params.size())
+
+    def write_embedding_to_file(self, fout):
+        for i in self.embeddings.parameters():
+            weights = i.data.numpy()
+        np.save(fout, weights)
+
+test_sentence = load_data("../data/train.txt").split()
+test_sentence = [e for e in test_sentence if e != " "]
+ngrams = []
+for i in range(len(test_sentence)-CONTEXT_SIZE):
+    tup = [test_sentence[j] for j in np.arange(i, i+CONTEXT_SIZE)]
+    ngrams.append((tup, test_sentence[i+CONTEXT_SIZE]))
+
+dic = load_json("../data/dictionary.json")
+vocab = set(test_sentence)
+vocab_size = len(vocab)
+print("Length of vocabulary: {}".format(len(vocab)))
+char2idx = {char: i for i, char in enumerate(vocab)}
+
+#import pickle
+
+#fin = "../data/wvec.hf"
+#hdf = h5py.File(fin, "r")
+#wvec = hdf["data"][:]
+#hdf.close()
+#wvec = torch.from_numpy(wvec)
+#print("wvec: ", wvec.size)
+
+losses = []
+loss_func = nn.NLLLoss()
+model = CBOW(vocab_size, EMBEDDING_DIM, CONTEXT_SIZE)
+optimizer = optim.SGD(model.parameters(), lr = 0.001)
+
+for epoch in range(400):
+    total_loss = 0
+
+    for context, target in ngrams:
+
+        context_idxs = torch.LongTensor([char2idx[w] for w in context])
+        target = torch.LongTensor([char2idx[target]])
+        model.zero_grad()
+
+        log_probs = model(context_idxs)
+        loss = loss_func(log_probs, target)
+
+        loss.backward()
+        optimizer.step()
+
+        total_loss+=loss.item()
+
+    print(total_loss)
+    losses.append(total_loss)
+
+
+#model.predict(['of', 'all', 'human'])
+model.write_embedding_to_file('embeddings.npy')
+#cluster_embeddings('embeddings.npy', 2)
